55aea1ff3a029fbb63affbd74132a4e786751f52
ismayilzada1 <130251565+ismayilzada1@users.noreply.github.com>
2024-04-09 22:04:32 +0400

    Add Sign In Page

---
+++ src/app/auth/sign-in/page.tsx
@@ -0,0 +1,58 @@
+// @use-client
+import React from 'react';
+import AuthLayout from '../auth-layout';
+import styles from "./sign-in.module.css"
+
+const SignInPage: React.FC = () => {
+    return (
+        <AuthLayout imageUrl="/assets/auth/img1.jpeg">
+            <div className="d-flex flex-column w-75">
+                <div className={"d-flex flex-row align-items-center justify-content-between"}>
+                    <div>
+                        <img className={styles.authHeaderLogo} src="/assets/otor-erp-logo.png" alt="Logo"/>
+                    </div>
+                    <div>
+                        <button type="button" className={`${styles.authHeaderButton} btn btn-outline-primary`}>Sign Up
+                        </button>
+                    </div>
+                </div>
+
+                <div className={"d-flex mt-5 flex-column align-items-start justify-content-around"}>
+                    <p className={`${styles.welcomeBackText}`}>Welcome back!!</p>
+                    <h3 className={`${styles.pleaseSignInText}`}>Please Sign In</h3>
+                </div>
+
+                <div className={"d-flex mt-3 flex-column align-items-start justify-content-around"}>
+
+                    <div className="input-group d-flex flex-column ">
+                        <label className={`${styles.inputText}`} htmlFor="email">Email Address</label>
+                        <input type="email" className={`${styles.authInput} form-control`} id="email"
+                               placeholder="Enter email adress"/>
+                    </div>
+
+                    <div className="input-group d-flex flex-column mt-4">
+                        <label className={`${styles.inputText}`} htmlFor="password">Password</label>
+                        <input type="password" className={`${styles.authInput} form-control`} id="password"
+                               placeholder="●●●●●●●●●●"/>
+                    </div>
+
+                </div>
+
+                <div  className={`${styles.rememberMeContainer} d-flex mt-3 flex-row align-items-center justify-content-between`}>
+                    <div>
+                        <input type="checkbox" id="rememberMe" className="form-check-input me-2"/>
+                        <label htmlFor="rememberMe" className="form-check-label">Remember Me</label>
+                    </div>
+                    <a className={`${styles.forgotPasswordText}`} href="">I forgot my password</a>
+                </div>
+
+                <div className="mt-3">
+                    <button type="submit" className={`${styles.signInButton} btn btn-primary w-75 mt-4`}>Sign In</button>
+                </div>
+
+            </div>
+        </AuthLayout>
+    );
+};
+
+export default SignInPage;
