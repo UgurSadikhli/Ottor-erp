940beaf9a4d8e1ee7d56bf0876ee35e5eaeccf32
Ughur Sadikhli <sadikhli.ughur@gmail.com>
2024-04-10 22:23:48 +0400

    dasboard page

---
+++ src/app/components/Card/card.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import "./card.css";
+
+export default function Card() {
+  return (
+    <>
+      <div className="card">
+        
+      </div>
+    </>
+  );
+}
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import "./dashboardP.css";
 import Table from "../components/Table/table";
+import Card from "../components/Card/card";
 
 export default function dashboard() {
   return (
@@ -8,7 +9,12 @@ export default function dashboard() {
       {/* <Table /> */}
       <h1>dashboard</h1>
       <div className="main_dashboard_div">
-        <div className="main_dashboard_top"></div>
+        <div className="main_dashboard_top">
+          <Card/>
+          <Card/>
+          <Card/>
+          <Card/>
+        </div>
         <div className="main_dashboard_beneath">
         <Table />
         <Table />
