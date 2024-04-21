9dbbfd1ce6f61fd1b7db513c71e34d2e351242e4
Vuqar <69174569+vugito@users.noreply.github.com>
2024-04-22 02:59:03 +0400

    'Add New Staff' page is already done

--- src/app/staff/add-new-staff/page.tsx
+++ src/app/staff/add-new-staff/page.tsx
@@ -1,11 +1,184 @@
-import React from 'react';
+"use client"
+import React, {useState} from 'react';
 import styles from "./AddNewStaff.module.css";
+import CustomButton from "@/app/components/Buttons/CustomButton/CustomButton";
+import ArrowRightIcon from "@/app/components/Icons/ArrowRightIcon/ArrowRightIcon";
+import MainLayout from "@/app/layouts/main-layout";
+import InputField from "@/app/components/Input/input";
+import AddPhotoIcon from "@/app/components/Icons/AddPhotoIcon/AddPhotoIcon";
+import uploadImg from "../../../../public/assets/otor-erp-logo.png";
+import SelectIndicator from "@/app/components/SelectIndicator/SelectIndicator";
+
+
+const SelectIndicatorOptions = {
+    gender: [
+        { value: "Male", label: "Male" },
+        { value: "Female", label: "Female" },
+    ],
+    role: [
+        { value: "All Staff", label: "All Staff" },
+        { value: "Admin Staff", label: "Admin Staff" },
+        { value: "IT Staff", label: "IT Staff" },
+        { value: "Human Resources Staff", label: "Human Resources Staff" },
+    ],
+    designation: [
+        { value: "Mr", label: "Mr" },
+        { value: "Ms", label: "Ms" },
+    ]
+}
+
 
 const AddNewStaff = () => {
+
+    const [selectedFile, setSelectedFile] = useState<File | null>(null);
+
+    const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const file = event.target.files?.[0];
+        if (file) {
+            setSelectedFile(file);
+        } else {
+            setSelectedFile(null);
+        }
+    };
+
+
+
+    // const handleFileChange = (event) => {
+    //     const file = event.target.files[0];
+    //     setSelectedFile(file);
+    // };
+
     return (
-        <div>
-            
-        </div>
+        <MainLayout>
+            <div className={styles.main}>
+                <div className={styles.top}>
+                    <CustomButton icon={<ArrowRightIcon color="black"/>} links="staff" label="Back"/>
+                </div>
+                <div className={styles.body}>
+                    <div className={styles.form}>
+                        <div className={styles.formTop}>
+                            <span className={styles.formTitle}>Add a New Staff</span>
+                        </div>
+                        <div className={styles.formBody}>
+                            <div className={styles.leftSide}>
+
+                                <div className={styles.inputContainer}>
+                                    <input onChange={handleFileChange} type="file" name="file" id="file" className={styles.input}/>
+                                    <label htmlFor="file" className={styles.inputLabelContainer}>
+                                        <div className={styles.inputLabelInner}>
+                                            <img className={selectedFile ? styles.image : styles.dNone}
+                                                 src={selectedFile ? URL.createObjectURL(selectedFile) : uploadImg}
+                                                 alt="uploaded photo"/>
+                                            <div className={selectedFile ? styles.indexZ : styles.photoFrameText}>
+                                                <AddPhotoIcon/>
+                                                <span>
+                                                    {
+                                                        selectedFile ? "Update photo" : "Upload photo"
+                                                    }
+                                                </span>
+                                            </div>
+                                        </div>
+                                    </label>
+                                    <div className={styles.inputDescription}>
+                                        <span className={styles.inputDescriptionTitle}>Allowed format</span>
+                                        <span className={styles.inputDescriptionText}>JPG, JPEG, and PNG</span>
+                                    </div>
+                                    <div className={styles.inputDescription}>
+                                        <span className={styles.inputDescriptionTitle}>Max file size</span>
+                                        <span className={styles.inputDescriptionText}>2MB</span>
+                                    </div>
+                                </div>
+
+                            </div>
+                            <div className={styles.rightSide}>
+                                <div className={styles.formBlock}>
+                                    <InputField
+                                        label="First name"
+                                        placeholder="Enter first name"
+                                        width={450}
+                                        height={54}
+                                    />
+                                    <InputField
+                                        label="Last name"
+                                        placeholder="Enter last name"
+                                        width={450}
+                                        height={54}
+                                    />
+                                </div>
+                                <div className={styles.formBlock}>
+                                    <InputField
+                                        label="Email address"
+                                        placeholder="Enter email address"
+                                        width={450}
+                                        height={54}
+                                    />
+                                    <InputField
+                                        label="Phone number"
+                                        placeholder="Enter phone number"
+                                        width={450}
+                                        height={54}
+                                    />
+                                </div>
+                                <div className={styles.formBlock}>
+                                    <SelectIndicator
+                                        options={SelectIndicatorOptions.gender}
+                                        placeholder="Select gender"
+                                        height={55}
+                                        width={450}
+                                        label="Gender"
+                                        showLabel={true}
+                                    />
+                                    <InputField
+                                        label="Phone number"
+                                        placeholder="Enter phone number"
+                                        width={450}
+                                        height={54}
+                                    />
+                                </div>
+                                <div className={styles.formBlock}>
+                                    <SelectIndicator
+                                        options={SelectIndicatorOptions.role}
+                                        placeholder="Select role"
+                                        height={55}
+                                        width={450}
+                                        label="Role"
+                                        showLabel={true}
+                                    />
+                                    <SelectIndicator
+                                        options={SelectIndicatorOptions.designation}
+                                        placeholder="Select designation"
+                                        height={55}
+                                        width={450}
+                                        label="Designation"
+                                        showLabel={true}
+                                    />
+                                </div>
+                                <div className={styles.formBlock}>
+                                    <InputField
+                                        label="Staff ID"
+                                        placeholder="Staff ID"
+                                        width={450}
+                                        height={54}
+                                    />
+                                    <InputField
+                                        label="Official email"
+                                        placeholder="Official email"
+                                        width={450}
+                                        height={54}
+                                    />
+                                </div>
+                            </div>
+                        </div>
+                        <div className={styles.formBottom}>
+                            <div className={styles.buttonContainer}>
+                                <button className={styles.button}>Add Staff</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </MainLayout>
+
     );
 };
 
