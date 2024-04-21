eacb18cc2a0755c1455edf6067b90b912e486935
Vuqar <69174569+vugito@users.noreply.github.com>
2024-04-21 17:48:06 +0400

    'Create Payslip' page is done

--- src/app/payroll/create-payslip/page.tsx
+++ src/app/payroll/create-payslip/page.tsx
@@ -8,6 +8,24 @@ import InputField from "@/app/components/Input/input";
 import DatePickerValue from "@/app/components/Calendar/calendar";
 import SelectIndicator from "@/app/components/SelectIndicator/SelectIndicator";
 
+const SelectIndicatorOptions = {
+    staffName: [
+        { value: "1", label: "1" },
+        { value: "2", label: "2" },
+        { value: "3", label: "3" },
+    ],
+    titles: [
+        { value: "1", label: "1" },
+        { value: "2", label: "2" },
+        { value: "3", label: "3" },
+    ],
+    levels: [
+        { value: "1", label: "1" },
+        { value: "2", label: "2" },
+        { value: "3", label: "3" },
+    ]
+};
+
 const CreatePayslip = () => {
     return (
         <MainLayout>
@@ -16,65 +34,148 @@ const CreatePayslip = () => {
                     <CustomButton icon={<ArrowRightIcon color="black"/>} links="payroll" label="Back" />
                 </div>
                 <div className={styles.body}>
-                    <div className={styles.form1}>
-                        <div className={styles.form1Top}>
-                            <span className={styles.formTopTW}>Create Budget</span>
-                            <span className={styles.formTopT}>Kindly fill in the form below to create a budget</span>
+                    <div className={styles.form}>
+                        <div className={styles.formTop}>
+                            <span className={styles.pageTitle}>Create Payslip</span>
+                            <span className={styles.formTitle}>Basic Information</span>
                         </div>
-                        <div className={styles.form1Bottom}>
-                            <div className={styles.form1H1}>
+                        <div className={styles.formBody}>
+                            <div className={styles.bodyContainer}>
+                                <SelectIndicator
+                                    options={SelectIndicatorOptions.staffName}
+                                    placeholder="Select staff"
+                                    height={55}
+                                    width={450}
+                                    label="Staff name"
+                                    showLabel={true}
+                                />
+                                <SelectIndicator
+                                    options={SelectIndicatorOptions.titles}
+                                    placeholder="Select title"
+                                    height={55}
+                                    width={450}
+                                    label="Title"
+                                    showLabel={true}
+                                />
+                                <SelectIndicator
+                                    options={SelectIndicatorOptions.levels}
+                                    placeholder="Select level"
+                                    height={55}
+                                    width={450}
+                                    label="Level"
+                                    showLabel={true}
+                                />
+                            </div>
+                        </div>
+                    </div>
+                    <div className={styles.form}>
+                        <div className={styles.formTop}>
+                            <span className={styles.formTitle}>Salary Structure</span>
+                        </div>
+                        <div className={styles.formBody}>
+                            <div className={styles.bodyContainer}>
+                                <InputField
+                                    label="Basic Salary"
+                                    placeholder="Enter amount"
+                                    width={450}
+                                    height={54}
+                                />
+                                <InputField
+                                    label="Housing allowance"
+                                    placeholder="Enter amount"
+                                    width={450}
+                                    height={54}
+                                />
+                                <InputField
+                                    label="Transport allowance"
+                                    placeholder="Enter amount"
+                                    width={450}
+                                    height={54}
+                                />
+                            </div>
+                            <div className={styles.bodyContainer}>
+                                <InputField
+                                    label="Utility allowance"
+                                    placeholder="Enter amount"
+                                    width={450}
+                                    height={54}
+                                />
+                                <InputField
+                                    label="Productivity allowance"
+                                    placeholder="Enter amount"
+                                    width={450}
+                                    height={54}
+                                />
+                                <InputField
+                                    label="Communication allowance"
+                                    placeholder="Enter amount"
+                                    width={450}
+                                    height={54}
+                                />
+                            </div>
+                            <div className={styles.bodyContainer}>
+                                <InputField
+                                    label="Inconvenience allowance"
+                                    placeholder="Enter amount"
+                                    width={450}
+                                    height={54}
+                                />
                                 <InputField
                                     label="Budget number"
-                                    placeholder="Enter item"
+                                    placeholder="Amount"
+                                    width={450}
+                                    height={54}
+                                />
+
+                            </div>
+                        </div>
+                    </div>
+                    <div className={styles.form}>
+                        <div className={styles.formTop}>
+                            <span className={styles.formTitle}>Deductions</span>
+                        </div>
+                        <div className={styles.formBody}>
+                            <div className={styles.bodyContainer}>
+                                <InputField
+                                    label="TAX/PAYE"
+                                    placeholder="Enter amount"
                                     width={450}
                                     height={54}
                                 />
                                 <InputField
-                                    label="Budget description"
-                                    placeholder="Enter description"
+                                    label="Employee pension"
+                                    placeholder="Enter amount"
                                     width={450}
                                     height={54}
                                 />
                                 <InputField
-                                    label="Budget amount"
-                                    placeholder="Enter amount in ₦"
+                                    label="Total deduction"
+                                    placeholder="Amount"
                                     width={450}
                                     height={54}
                                 />
                             </div>
-                            <div className={styles.form1H2}>
-                                <div className={styles.form1SpanDatePickerValue}>
-                                    <span className={styles.formTopDate}>Date</span>
-                                    <DatePickerValue width={450}  />
-                                </div>
-                                {/*<SelectIndicator*/}
-                                {/*    options={SelectIndicatoroptions}*/}
-                                {/*    placeholder="Select option"*/}
-                                {/*    height={55}*/}
-                                {/*    width={450}*/}
-                                {/*    label="Receiving office"*/}
-                                {/*    showLabel={true}*/}
-                                {/*/>*/}
-
-                            </div>
-                            <div className={styles.form1H3}>
-                                <button className={styles.buttons}>Create Budget</button>
-                            </div>
-
-                        </div>
-
-                    </div>
-                    <div className={styles.form2}>
-                        <div className={styles.form2Top}>
-                            <span className={styles.formTopTW}>Budget Request</span>
-                        </div>
-                        <div className={styles.form2Bottom}>
-                            <button className={styles.buttons2}>Submit for Approval</button>
                         </div>
                     </div>
-
+                    <div className={styles.form}>
+                        <div className={styles.formTop}>
+                            <span className={styles.formTitle}>Net Salary</span>
+                        </div>
+                        <div className={styles.formBody}>
+                            <div className={styles.bodyContainer}>
+                                <InputField
+                                    label="Net salary"
+                                    placeholder="Amount"
+                                    width={450}
+                                    height={54}
+                                />
+                            </div>
+                        </div>
+                        <div className={styles.formBottom}>
+                            <button className={styles.buttons}>Create Payslip</button>
+                        </div>
+                    </div>
                 </div>
-
             </div>
         </MainLayout>
     );
