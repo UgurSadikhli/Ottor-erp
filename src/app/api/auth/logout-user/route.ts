0ca33806772e7f67ec3ce4de8209fa575241f654
ismayilzada1 <130251565+ismayilzada1@users.noreply.github.com>
2024-04-22 17:43:59 +0400

    Api logout

---
+++ src/app/api/auth/logout-user/route.ts
@@ -0,0 +1,25 @@
+import { NextResponse } from "next/server";
+import fs from "fs";
+import { cookies } from 'next/headers'
+
+
+const dbPath = "db.json";
+
+export async function POST(req: Request) {
+    const reqObject = await req.json();
+    const db = JSON.parse(fs.readFileSync(dbPath, "utf-8"));
+
+    try {
+        cookies().set('auth-token','');
+
+        return new NextResponse(null, {
+            headers: {
+                "Content-Type": "application/json",
+            },
+            status: 200,
+        });
+
+    } catch (error) {
+        return NextResponse.json({ error: "An error occurred" }, { status: 500 });
+    }
+}
