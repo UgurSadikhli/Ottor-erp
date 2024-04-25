10b594555b5465edd19409493e1c7f1cc02c156b
ahmadzada111 <ahmad.ahmadzada999@gmail.com>
2024-04-25 20:28:15 +0400

    Add sql and fixed bugs

---
+++ prisma/migrations/20240425154557_third/migration.sql
@@ -0,0 +1,24 @@
+-- RedefineTables
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_User" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "email" TEXT NOT NULL,
+    "officialEmail" TEXT NOT NULL,
+    "emailConfirmed" BOOLEAN NOT NULL DEFAULT false,
+    "name" TEXT NOT NULL,
+    "surname" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "phoneNumber" TEXT NOT NULL,
+    "gender" TEXT NOT NULL,
+    "role" TEXT NOT NULL,
+    "designation" TEXT NOT NULL,
+    "staffId" TEXT NOT NULL,
+    "profileImage" BLOB
+);
+INSERT INTO "new_User" ("designation", "email", "gender", "id", "name", "officialEmail", "password", "phoneNumber", "profileImage", "role", "staffId", "surname") SELECT "designation", "email", "gender", "id", "name", "officialEmail", "password", "phoneNumber", "profileImage", "role", "staffId", "surname" FROM "User";
+DROP TABLE "User";
+ALTER TABLE "new_User" RENAME TO "User";
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+CREATE UNIQUE INDEX "User_officialEmail_key" ON "User"("officialEmail");
+PRAGMA foreign_key_check;
+PRAGMA foreign_keys=ON;
