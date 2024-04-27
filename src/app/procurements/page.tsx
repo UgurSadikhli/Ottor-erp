76e57b7e0fd3ddb74365e08963d7751459f1eb75
ahmadzada111 <ahmad.ahmadzada999@gmail.com>
2024-04-27 21:48:54 +0400

    Add procurements page

--- src/app/procurements/page.tsx
+++ src/app/procurements/page.tsx
@@ -1,10 +1,160 @@
-import Image from "next/image";
-import MainLayout from '../layouts/main-layout'
+import MainLayout from "../layouts/main-layout";
+import React from "react";
+import styles from "./procurements.module.css";
+import Card from "@/app/components/Card/card";
+import ArrowUpIcon from "@/app/components/Icons/ArrowUpIcon/ArrowUpIcon";
+import ArrowDownIcon from "@/app/components/Icons/ArrowDownIcon/ArrowDownIcon";
+import Link from "next/link";
+import CustomTable from "@/app/components/Table/CustomTable/CustomTable";
+import Table from "../components/Table/table";
+import BagIcon from "../components/Icons/BagIcon/BagIcon";
+import Header from "../components/Header/header";
+import LogisticsIcon from "../components/Icons/LogisticsIcon/LogisticsIcon";
 
 export default function procurements() {
+  const headers = [
+    { id: 1, headerName: "S/N" },
+    { id: 2, headerName: "Item" },
+    { id: 3, headerName: "Qty" },
+    { id: 4, headerName: "Amount" },
+    { id: 5, headerName: "Requested By" },
+    { id: 6, headerName: "Sent to" },
+    { id: 7, headerName: "Date" },
+    { id: 8, headerName: "Status" },
+    { id: 8, headerName: "Action" },
+  ];
+
+  const innerData = [
+    {
+      id: 1,
+      Item: "Office chairs",
+      Qty: "20",
+      Amount: "360,000.00",
+      Requested: "Otor John",
+      Sent: "Faruk Hashim",
+      Date: "21/11/2022",
+      Status: "Pending",
+      Action: "View more",
+    },  {
+      id: 2,
+      Item: "Office chairs",
+      Qty: "20",
+      Amount: "360,000.00",
+      Requested: "Otor John",
+      Sent: "Faruk Hashim",
+      Date: "21/11/2022",
+      Status: "Pending",
+      Action: "View more",
+    },  {
+      id: 3,
+      Item: "Office chairs",
+      Qty: "20",
+      Amount: "360,000.00",
+      Requested: "Otor John",
+      Sent: "Faruk Hashim",
+      Date: "21/11/2022",
+      Status: "Approved",
+      Action: "View more",
+    },  {
+      id: 4,
+      Item: "Office chairs",
+      Qty: "20",
+      Amount: "360,000.00",
+      Requested: "Otor John",
+      Sent: "Faruk Hashim",
+      Date: "21/11/2022",
+      Status: "Approved",
+      Action: "View more",
+    },  {
+      id: 5,
+      Item: "Office chairs",
+      Qty: "20",
+      Amount: "360,000.00",
+      Requested: "Otor John",
+      Sent: "Faruk Hashim",
+      Date: "21/11/2022",
+      Status: "Pending",
+      Action: "View more",
+    }, {
+      id: 6,
+      Item: "Office chairs",
+      Qty: "20",
+      Amount: "360,000.00",
+      Requested: "Otor John",
+      Sent: "Faruk Hashim",
+      Date: "21/11/2022",
+      Status: "Rejected",
+      Action: "View more",
+    }, {
+      id: 7,
+      Item: "Office chairs",
+      Qty: "20",
+      Amount: "360,000.00",
+      Requested: "Otor John",
+      Sent: "Faruk Hashim",
+      Date: "21/11/2022",
+      Status: "Approved",
+      Action: "View more",
+    },
+  
+  ];
   return (
-    <MainLayout label="View, search for and add new staff" title=" All Staff">
-      <h1>procurements</h1>
+    <MainLayout
+      label="Request for, and view all requested procurements."
+      title=" Procurement"
+    >
+      <div className={styles.container}>
+        <div className={styles.statisticsContainer}>
+          <Card
+            number={"350"}
+            title="Total request made"
+            description="50 more than last year"
+            icon={<BagIcon color={"#248CD8"} />}
+            arrowIcon={<ArrowUpIcon color="#10A242" />}
+            backgroundColor="#E8F5FF"
+          />
+          <Card
+            number={"8,000,000"}
+            title="Total cost incurred"
+            description=""
+            icon={<BagIcon color={"#A601FF"} />}
+            backgroundColor="#F9EFFF"
+          />
+          <Card
+            number={"20"}
+            title="Pending request"
+            description=""
+            icon={<BagIcon color={"#F29425"} />}
+            backgroundColor="#FFF4E8"
+          />
+
+          <Card
+            number={"330"}
+            title="Approved request"
+            description="2% more than last year"
+            icon={<BagIcon color={"#10A242"} />}
+            arrowIcon={<ArrowUpIcon color="#ED3237" />}
+            backgroundColor="#ECFFF2"
+          />
+        </div>
+        <div className={styles.createBudgetContainer}>
+          <div>
+            <span className={styles.title}>Procurement request</span>
+          </div>
+          <div>
+            <Link href="/procurements/procurements-request">
+              <button className={styles.buttons}>Make Procurement Request</button>
+            </Link>
+          </div>
+        </div>
+        <div className={styles.budgetHistoryContainer}>
+          <CustomTable
+            blockTitle="Procurement Request"
+            headers={headers}
+            innerData={innerData}
+          />
+        </div>
+      </div>
     </MainLayout>
   );
 }
---
+++ src/app/procurements/procurements-request/page.tsx
@@ -0,0 +1,240 @@
+"use client";
+import Image from "next/image";
+import MainLayout from "../../layouts/main-layout";
+import React, { useState } from "react";
+import styles from "./procurements-request.module.css";
+import IconButton from "@/app/components/IconButton/iconButton";
+import SelectIndicator from "../../components/SelectIndicator/SelectIndicator";
+import InputField from "../../components/Input/input";
+import FocusedRingTextarea from "../../components/TextArea/textArea";
+import ArrowRightIcon from "@/app/components/Icons/ArrowRightIcon/ArrowRightIcon";
+import Link from "next/link";
+import DatePickerValue from "@/app/components/Calendar/calendar";
+import CustomButton from "@/app/components/Buttons/CustomButton/CustomButton";
+import Header from "@/app/components/Header/header";
+import CustomTable from "@/app/components/Table/CustomTable/CustomTable";
+import LogisticsIcon from "@/app/components/Icons/LogisticsIcon/LogisticsIcon";
+
+const SelectIndicatoroptions = [
+  { value: "1", label: "1" },
+  { value: "2", label: "2" },
+  { value: "3", label: "3" },
+];
+const headers = [
+  { id: 1, headerName: "S/N" },
+  { id: 2, headerName: "Item" },
+  { id: 3, headerName: "Quantity" },
+  { id: 4, headerName: "Date" },
+  { id: 5, headerName: "Unit Price (₦)" },
+  { id: 6, headerName: "Total Price (₦)" },
+  { id: 7, headerName: "VAT %" },
+  { id: 8, headerName: "VAT Amount (₦)" },
+  { id: 9, headerName: "Gross Amount (₦)" },
+
+];
+
+const innerData = [
+  {
+    id: 1,
+    Item: "Office chairs",
+    Quantity: "25",
+    Date: "22/11/2022",
+    Unit: "100,000.00",
+    Total: "2,500,000.00",
+    VAT: "7.50%",
+    Amount: "187,500.00",
+    Gross: "2,687,500.00",
+  },
+];
+const ProcurementsRequest = () => {
+  return (
+    <MainLayout
+      label="Request for, and view all requested procurements."
+      title=" Procurement"
+    >
+      <div className={styles.main}>
+        <div className={styles.top}>
+          <CustomButton
+            icon={<ArrowRightIcon color="black" />}
+            links="procurements"
+            label="Back"
+          />
+        </div>
+        <div className={styles.body}>
+          <div className={styles.form1}>
+            <div className={styles.form1Top}>
+              <span className={styles.formTopText1}>Procurement Request</span>
+              <span className={styles.formTopText2}>
+                Kindly fill in the form below to submit a logistics request
+              </span>
+            </div>
+            <div className={styles.form1Body}>
+              <div className={styles.form1BodyTop}>
+                <div className={styles.form1BodyTopInput1}>
+                  <InputField
+                    label="Item"
+                    placeholder="Enter item name"
+                    width={470}
+                    height={54}
+                  />
+                  <InputField
+                    label="Quantity"
+                    placeholder="Enter quantity"
+                    width={470}
+                    height={54}
+                  />
+                  <div className={styles.datePickerDiv}>
+                    <span className={styles.datePickertext}>Date</span>
+                    <DatePickerValue width={470} />
+                  </div>
+                </div>
+                <div className={styles.form1BodyTopInput2}>
+                  <InputField
+                    label="Unit price"
+                    placeholder="Enter amount"
+                    width={470}
+                    height={54}
+                  />
+                  <InputField
+                    label="Total price"
+                    placeholder="Enter amount"
+                    width={470}
+                    height={54}
+                  />
+                  <InputField
+                    label="Requested by"
+                    placeholder="Otor John"
+                    width={470}
+                    height={54}
+                  />
+                </div>
+                <div className={styles.form1BodyTopInput2}>
+                  <SelectIndicator
+                    options={SelectIndicatoroptions}
+                    placeholder="Select option"
+                    height={54}
+                    width={470}
+                    label="Sent to"
+                    showLabel={true}
+                  />
+                  <SelectIndicator
+                    options={SelectIndicatoroptions}
+                    placeholder="Select option"
+                    height={54}
+                    width={470}
+                    label="Add Attachement"
+                    showLabel={true}
+                  />
+                  <SelectIndicator
+                    options={SelectIndicatoroptions}
+                    placeholder="Select option"
+                    height={54}
+                    width={470}
+                    label="Attachement type"
+                    showLabel={true}
+                  />
+                </div>
+              </div>
+              <div className={styles.form1BodyBottom}>
+                <button className={styles.buttons}>
+                  Attach Payment Voucher
+                </button>
+              </div>
+            </div>
+          </div>
+          <div className={styles.form2}>
+            <div className={styles.form2Top}>
+              <span className={styles.formTopText1}>Payment Voucher</span>
+              <div className={styles.formTable}>
+                <CustomTable headers={headers} innerData={innerData} />
+              </div>
+            </div>
+            <div className={styles.form2Body}>
+              <div className={styles.form2BodyMemo}>
+                  <span className={styles.form2BodyText1}>
+                    Beneficiary Payment Details
+                  </span>
+                  <div className={styles.form2BodyInputSignature}>
+                    <div className={styles.form2BodyInputs}>
+                      <InputField
+                        label="Account name"
+                        placeholder="Otor John Stephen"
+                        width={470}
+                        height={54}
+                      />
+                      <InputField
+                        label="Account number"
+                        placeholder="Enter number"
+                        width={470}
+                        height={54}
+                      />
+                      <InputField
+                        label="Bank name"
+                        placeholder="Enter bank name"
+                        width={470}
+                        height={54}
+                      />
+                    </div>
+                  </div>
+              </div>
+           
+            <div className={styles.form2BodyMemo}>
+              <span className={styles.form2BodyText1}>Memo Activities</span>
+              <div>
+                <div className={styles.bottomlast}>
+                  <InputField
+                    label="Initiated by"
+                    placeholder="Otor John"
+                    width={470}
+                    height={54}
+                  />
+                  <SelectIndicator
+                    options={SelectIndicatoroptions}
+                    placeholder="Select option"
+                    height={54}
+                    width={470}
+                    label="Verified by"
+                    showLabel={true}
+                  />
+                  <SelectIndicator
+                    options={SelectIndicatoroptions}
+                    placeholder="Select option"
+                    height={54}
+                    width={470}
+                    label="Approved by"
+                    showLabel={true}
+                  />
+                </div>
+                <div className={styles.form2BodySignature}>
+                  <div className={styles.form2BodySignature1}>
+                    <hr className={styles.form2BodySignatureHR} />
+                    <span className={styles.form2BodySignatureText}>
+                      Verifier Signature
+                    </span>
+                  </div>
+                  <div className={styles.form2BodySignature2}>
+                    <hr className={styles.form2BodySignatureHR} />
+                    <span className={styles.form2BodySignatureText}>
+                      Authorizer Signature
+                    </span>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className={styles.form2BodyButtons}>
+            <Link href={"/procurements/procurements-details"}>
+              <button className={styles.buttons1}>
+                Save and Send for Approval
+              </button>
+            </Link>
+            <button className={styles.buttons2}>Save</button>
+          </div>
+          </div>
+        </div>
+      </div>
+    </MainLayout>
+  );
+};
+
+export default ProcurementsRequest;
